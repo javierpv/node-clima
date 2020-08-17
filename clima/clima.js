035a19f64ec48f70670d3c872e0dcd8f497140cc
Javier <jpadron@telefonica.net>
2020-08-17 11:06:37 +0200

    fin

--- clima/clima.js
+++ clima/clima.js
@@ -2,8 +2,7 @@ const axios = require('axios');
 
 const getClima = async (lat, lon) => {
 
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4a2b0015394865bab7e79cdda684fcd8
-    &units=metric`);
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=4a2b0015394865bab7e79cdda684fcd8&units=metric`);
 
     return resp.data.main.temp;
 
